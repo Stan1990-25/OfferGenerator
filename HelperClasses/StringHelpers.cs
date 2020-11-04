991bb463cb313ad548b4c0286332c462580decc2
Stan1990-25 <63590563+Stan1990-25@users.noreply.github.com>
2020-11-04 16:06:04 +0200

    Add files via upload

---
+++ HelperClasses/StringHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TMOffersClients
+{
+    /// <summary>
+    /// Helper class for strings
+    /// </summary>
+    public static class StringHelpers
+    {
+        /// <summary>
+        /// Indicates whether a string contains a certain substring
+        /// </summary>
+        /// <param name="source">The source string</param>
+        /// <param name="value">The string that will be looked for</param>
+        /// <param name="comparison">Comparison options</param>
+        /// <returns></returns>
+        public static bool Contains(this string source, string value, StringComparison comparison)
+        {
+            return source.IndexOf(value, comparison) > -1;
+        }
+    }
+}
