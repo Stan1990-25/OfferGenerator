13269b3bf3a2912a53eb92717e856123babbeda2
Stan1990-25 <63590563+Stan1990-25@users.noreply.github.com>
2020-11-04 16:07:39 +0200

    Add files via upload

---
+++ Migrations/20201005134258_PKMeters.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TMOffersClients.Migrations
+{
+    public partial class PKMeters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OffersPorducts",
+                table: "OffersPorducts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OffersPorducts",
+                table: "OffersPorducts",
+                columns: new[] { "OfferID", "ProductID", "Meters" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OffersPorducts",
+                table: "OffersPorducts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OffersPorducts",
+                table: "OffersPorducts",
+                columns: new[] { "OfferID", "ProductID" });
+        }
+    }
+}
---
+++ Migrations/20201006125214_AddApprovedOffer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TMOffersClients.Migrations
+{
+    public partial class AddApprovedOffer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Offers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Offers");
+        }
+    }
+}
