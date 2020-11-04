1ec2946b1cabe4e55c6e664a584d9783014785a7
Stan1990-25 <63590563+Stan1990-25@users.noreply.github.com>
2020-11-04 16:27:32 +0200

    Add files via upload

---
+++ Utilities/ValidEmailDomainAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement
+{
+    public class ValidEmailDomainAttribute : ValidationAttribute
+    {
+        private readonly string mAllowedDomain;
+
+        public ValidEmailDomainAttribute(string allowedDomain) : base(allowedDomain)
+        {
+            mAllowedDomain = allowedDomain;
+        }
+
+        public override bool IsValid(object value)
+        {
+            string valToString = value.ToString().ToLower();
+            return valToString.EndsWith(mAllowedDomain.ToLower());
+        }
+    }
+}
